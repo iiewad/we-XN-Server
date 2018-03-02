57858c4c5dc029539582375ba91c02f67ca070d7
iewad <lidawei.me@gmail.com>
2018-03-02 19:31:55 +0800

    添加API/Base控制器，并配置Api Version

---
+++ app/controllers/api/base_controller.rb
@@ -0,0 +1,21 @@
+class Api::BaseController < ApplicationController
+  include ApiHelper
+  before_action :sanitize
+
+  rescue_from ActionController::ParameterMissing, with: :parameter_missing
+  rescue_from ActionController::RoutingError,     with: :not_found
+  rescue_from ActiveRecord::RecordNotFound,       with: :not_found
+  rescue_from ActionController::UnknownFormat,    with: :not_found
+
+  def bad_request(errors = [])
+    @errors ||= if errors.is_a? Array
+                  errors
+                else
+                  [errors.to_s]
+                end
+    Rails.logger.error "BadRequest: #{errors}"
+    render 'api/base/error_message'
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+
+  api vendor_string: 'api', default_version: 1 do
+    version 1 do
+      cache as: 'v1' do
+      end
+    end
+  end
+
 end
