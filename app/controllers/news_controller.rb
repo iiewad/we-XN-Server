b273cd4a7d6b19fbb338917df8db023e998e6497
iewad <lidawei.me@gmail.com>
2018-03-10 11:54:15 +0800

    修复新闻列表API异常

--- app/controllers/news_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::NewsController < Api::BaseController
-
-  include Api::ApiHelper
-
-  def index
-    request_hunau_api_get_news_list
-  end
-
-end
--- app/models/news.rb
+++ app/models/news.rb
@@ -2,4 +2,5 @@ class News < ApplicationRecord
   validates :news_id, uniqueness: true
 
   DEFAULT_PER = 10
+  paginates_per DEFAULT_PER
 end
